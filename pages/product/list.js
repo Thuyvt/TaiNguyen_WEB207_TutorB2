ef9a28f9a93a7318cfe5558be86e38861f077704
thuyvt <thuyvt1005@gmail.com>
2024-03-28 21:26:47 +0700

    tutor

--- pages/product/list.js
+++ pages/product/list.js
@@ -1,2 +1,29 @@
 spaApp.controller('dsSpCtrl', function ($scope, $http) {
+    $url = "http://localhost:3000/san-pham";
+
+    $scope.dsSanPham = [];
+
+    // Call api lấy thông tin danh sách sản phẩm
+    $http({
+        method: "GET",
+        url: $url
+    }).then(function (response) {
+        $scope.dsSanPham = response.data;
+        console.log($scope.dsSanPham);
+    })
+
+    // Bắt sự kiện bấm nút xóa
+    $scope.onClickDelete = function (idsp) {
+        //  Xác nhận trước khi xóa
+
+        // Call api để xóa sản phẩm
+        $http({
+            method: "DELETE",
+            url: $url + "/" + idsp
+        }).then(function() {
+            alert('Xóa sản phẩm thành công');
+        }).catch(function(error) {
+            alert('Có lỗi xảy ra');
+        })
+    }
 });

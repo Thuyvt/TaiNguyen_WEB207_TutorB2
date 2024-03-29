78f9112c5b2326351e9aba1bf592229fe90186e5
Thuyvt <thuyvt1005@gmail.com>
2024-03-29 09:02:45 +0700

    Add files via upload

--- pages/product/create.js
+++ pages/product/create.js
@@ -1,3 +1,20 @@
+
+var apiUrl = "http://localhost:3000/san-pham";
 spaApp.controller('createSpCtrl', function ($scope, $http) {
-    console.log('createSpCtrl');
+    $scope.ten = "";
+    $scope.anh = "";
+    $scope.tenDanhMuc = "";
+
+    $scope.addSp = function(){
+        $scope.addObj = {
+            "ten": $scope.ten,
+            "anh": $scope.anh,
+            "tenDanhMuc": $scope.tenDanhMuc
+        }
+        $http.post(apiUrl, $scope.addObj).then(
+            function(res){
+                alert("thêm thành công");
+                return document.location = "#!list-product";
+            })
+    }
 });
--- pages/product/edit.js
+++ pages/product/edit.js
@@ -1,2 +1,35 @@
+
+var apiUrl = "http://localhost:3000/san-pham";
 spaApp.controller('editSpCtrl', function ($scope, $http ,$routeParams) {
+    $scope.getObj = [];
+    $scope.id = $routeParams.id;
+
+    // lấy 1 đối tượng
+    $http.get(`${apiUrl}/${$scope.id}`).then(
+        function(res){
+            if(res.status = 200){
+                $scope.getObj = res.data;
+                console.log(res.data);
+            }
+        }, function(res){
+            alert("không lấy được dữ liệu");
+        }
+    )
+
+    // xử lý button sửa
+    $scope.updateSp = function(){
+        $scope.editParams = {
+            "ten": $scope.getObj.ten,
+            "anh": $scope.getObj.anh,
+            "tenDanhMuc": $scope.getObj.tenDanhMuc
+        }
+        $http.put(`${apiUrl}/${$scope.id}`, $scope.editParams).then(
+            function(){
+                alert("Sửa thành công");
+                return document.location = "#!list-product";
+            }, function(){
+                alert("sửa không thành công")
+            }
+        )
+    }
 });
